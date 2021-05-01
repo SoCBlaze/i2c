1f7c95244f95118d13462bb2576fc54f1a009b13
t123yh <t123yh@outlook.com>
2021-05-01 16:09:57 +0800

    Change wishbone to fake apb

--- rtl/verilog/i2c_master_top.v
+++ rtl/verilog/i2c_master_top.v
@@ -77,48 +77,25 @@
 `include "i2c_master_defines.v"
 
 module i2c_master_top(
-	wb_clk_i, wb_rst_i, wb_adr_i, wb_dat_i, wb_dat_o,
-	wb_we_i, wb_stb_i, wb_cyc_i, wb_ack_o, wb_inta_o,
-	scl_pad_i, scl_pad_o, scl_padoen_o, sda_pad_i, sda_pad_o, sda_padoen_o );
+	input               clk,
+	input               rst,
+	output reg          irq,
 
-	// parameters
-	parameter ARST_LVL = 1'b0; // asynchronous reset level
+	input       [2:0]   csr_address,
+	input               csr_read,
+	output reg  [7:0]   csr_readdata,
+	input               csr_write,
+	input       [7:0]   csr_writedata,
 
-	//
-	// inputs & outputs
-	//
-
-	// wishbone signals
-	input        wb_clk_i;     // master clock input
-	input        wb_rst_i;     // synchronous active high reset
-	input  [2:0] wb_adr_i;     // lower address bits
-	input  [7:0] wb_dat_i;     // databus input
-	output [7:0] wb_dat_o;     // databus output
-	input        wb_we_i;      // write enable input
-	input        wb_stb_i;     // stobe/core select signal
-	input        wb_cyc_i;     // valid bus cycle input
-	output       wb_ack_o;     // bus cycle acknowledge output
-	output       wb_inta_o;    // interrupt request signal output
-
-	reg [7:0] wb_dat_o;
-	reg wb_ack_o;
-	reg wb_inta_o;
-
-	// I2C signals
-	// i2c clock line
-	input  scl_pad_i;       // SCL-line input
-	output scl_pad_o;       // SCL-line output (always 1'b0)
-	output scl_padoen_o;    // SCL-line output enable (active low)
+	input  scl_pad_i,       // SCL-line input
+	output scl_pad_o,       // SCL-line output (always 1'b0)
+	output scl_padoen_o,    // SCL-line output enable (active low)
 
 	// i2c data line
-	input  sda_pad_i;       // SDA-line input
-	output sda_pad_o;       // SDA-line output (always 1'b0)
-	output sda_padoen_o;    // SDA-line output enable (active low)
-
-
-	//
-	// variable declarations
-	//
+	input  sda_pad_i,       // SDA-line input
+	output sda_pad_o,       // SDA-line output (always 1'b0)
+	output sda_padoen_o    // SDA-line output enable (active low)
+);
 
 	// registers
 	reg  [15:0] prer; // clock prescale register
@@ -148,54 +125,47 @@ module i2c_master_top(
 	// module body
 	//
 
-	// generate wishbone signals
-	wire wb_wacc = wb_cyc_i & wb_stb_i & wb_we_i;
-
-	// generate acknowledge output signal
-	always @(posedge wb_clk_i)
-	  wb_ack_o <=  wb_cyc_i & wb_stb_i & ~wb_ack_o; // because timing is always honored
-
 	// assign DAT_O
-	always @(posedge wb_clk_i)
+	always @(posedge clk)
 	begin
-	  case (wb_adr_i) // synopsys parallel_case
-	    3'b000: wb_dat_o <=  prer[ 7:0];
-	    3'b001: wb_dat_o <=  prer[15:8];
-	    3'b010: wb_dat_o <=  ctr;
-	    3'b011: wb_dat_o <=  rxr; // write is transmit register (txr)
-	    3'b100: wb_dat_o <=  sr;  // write is command register (cr)
-	    3'b101: wb_dat_o <=  txr;
-	    3'b110: wb_dat_o <=  cr;
-	    3'b111: wb_dat_o <=  0;   // reserved
+	  case (csr_address) // synopsys parallel_case
+	    3'b000: csr_readdata <=  prer[ 7:0];
+	    3'b001: csr_readdata <=  prer[15:8];
+	    3'b010: csr_readdata <=  ctr;
+	    3'b011: csr_readdata <=  rxr; // write is transmit register (txr)
+	    3'b100: csr_readdata <=  sr;  // write is command register (cr)
+	    3'b101: csr_readdata <=  txr;
+	    3'b110: csr_readdata <=  cr;
+	    3'b111: csr_readdata <=  0;   // reserved
 	  endcase
 	end
 
 	// generate registers
-	always @(posedge wb_clk_i)
-	  if (wb_rst_i)
+	always @(posedge clk)
+	  if (rst)
 	    begin
 	        prer <=  16'hffff;
 	        ctr  <=   8'h0;
 	        txr  <=   8'h0;
 	    end
 	  else
-	    if (wb_wacc)
-	      case (wb_adr_i) // synopsys parallel_case
-	         3'b000 : prer [ 7:0] <=  wb_dat_i;
-	         3'b001 : prer [15:8] <=  wb_dat_i;
-	         3'b010 : ctr         <=  wb_dat_i;
-	         3'b011 : txr         <=  wb_dat_i;
+	    if (csr_write)
+	      case (csr_address) // synopsys parallel_case
+	         3'b000 : prer [ 7:0] <=  csr_writedata;
+	         3'b001 : prer [15:8] <=  csr_writedata;
+	         3'b010 : ctr         <=  csr_writedata;
+	         3'b011 : txr         <=  csr_writedata;
 	         default: ;
 	      endcase
 
 	// generate command register (special case)
-	always @(posedge wb_clk_i)
-	  if (wb_rst_i)
+	always @(posedge clk)
+	  if (rst)
 	    cr <=  8'h0;
-	  else if (wb_wacc)
+	  else if (csr_write)
 	    begin
-	        if (core_en & (wb_adr_i == 3'b100) )
-	          cr <=  wb_dat_i;
+	        if (core_en & (csr_address == 3'b100) )
+	          cr <=  csr_writedata;
 	    end
 	  else
 	    begin
@@ -221,8 +191,8 @@ module i2c_master_top(
 
 	// hookup byte controller block
 	i2c_master_byte_ctrl byte_controller (
-		.clk      ( wb_clk_i     ),
-		.rst      ( wb_rst_i     ),
+		.clk      ( clk     ),
+		.rst      ( rst     ),
 		.ena      ( core_en      ),
 		.clk_cnt  ( prer         ),
 		.start    ( sta          ),
@@ -245,8 +215,8 @@ module i2c_master_top(
 	);
 
 	// status register block + interrupt request signal
-	always @(posedge wb_clk_i)
-        if (wb_rst_i)
+	always @(posedge clk)
+        if (rst)
 	    begin
 	        al       <=  1'b0;
 	        rxack    <=  1'b0;
@@ -262,11 +232,11 @@ module i2c_master_top(
 	    end
 
 	// generate interrupt request signals
-	always @(posedge wb_clk_i)
-	  if (wb_rst_i)
-	    wb_inta_o <=  1'b0;
+	always @(posedge clk)
+	  if (rst)
+	    irq <=  1'b0;
 	  else
-	    wb_inta_o <=  irq_flag && ien; // interrupt signal is only generated when IEN (interrupt enable bit is set)
+	    irq <=  irq_flag && ien; // interrupt signal is only generated when IEN (interrupt enable bit is set)
 
 	// assign status register bits
 	assign sr[7]   = rxack;
